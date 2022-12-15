a7d2a88c5444edb80d8feed4a13cf2675cf7d4dd
Chris Sharp <sharpchr@gmail.com>
2022-12-15 16:47:04 +0000

    Exercises for Day 2

---
+++ Life in Weeks.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+timeleft = 90-int(age)
+x=timeleft*365
+y=timeleft*52
+z=timeleft*12
+
+print(f"You have {x} days, {y} weeks, and {z} months left.")
